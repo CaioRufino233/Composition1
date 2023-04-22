292bfc7b2ee2158b0e7e99b872affdb87b1da9ef
CaioRufino233 <130802702+CaioRufino233@users.noreply.github.com>
2023-04-21 23:07:06 -0300

    Fix the simpleDateFormatter

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,7 @@ import entities.enums.WorkerLevel;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the departament name: ");
